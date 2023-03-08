404b19b695cfa5ace839a2f8ca61f2b8c14c63fe
Zeta002 <angeloportelli05@gmail.com>
2023-03-08 17:20:11 +0100

    + Class inventaire (En cours de développement) 08/03/2023 17:20

--- RpgGame/src/Test.java
+++ RpgGame/src/Test.java
@@ -1,6 +1,7 @@
 import character.Character;
 import equipements.helmets.*;
 import equipements.rings.*;
+import inventory.Inventory;
 import equipements.*;
 
 public class Test {
@@ -13,5 +14,14 @@ public class Test {
 		System.out.println(c1);
 		c1.showStuff();
 		
-	}	
+		// creation of an inventory
+		System.out.println("********");
+		Inventory i1 = new Inventory();
+		System.out.println(i1);
+		
+		// create and add a stuff to it
+		Stuff h1 = new Helmet();
+		i1.addItem(h1);
+		System.out.println(i1);
+	}
 }

d2fe3eb7a53e3ce8c254502938e7c49a0c9cfddd
Quentin <quentinb2004@gmail.com>
2023-03-08 18:23:34 +0100

    changement dans la classe Inventaire et Character pour faire fonctionner l'inventaire

--- RpgGame/src/inventory/Inventory.java
+++ RpgGame/src/inventory/Inventory.java
@@ -1,18 +1,15 @@
 package inventory;
 
-import character.*;
 import equipements.*;
 
 public class Inventory {
+	
 	private Stuff[] stuffs;
 	private int size;
 
 	public Inventory() {
 		this.size = 64;
 		this.stuffs = new Stuff[size];
-		for (int i = 0; i < size; ++i) {
-			this.stuffs[i] = new Stuff();
-		}
 	}
 
 	public Inventory(int size, Stuff[] items) {
@@ -23,36 +20,23 @@ public class Inventory {
 		}
 	}
 
-	public String showStockage(Stuff[] items) {
-		int sizeRemaining = size - items.length;
+	public String showStockage() {
+		int sizeRemaining = size - stuffs.length;
 		return "" + sizeRemaining + "/" + size;
 	}
 
-	public String show() {
-		if (stuffs.length == 0)
-			return "You don't have any items in your inventory at the moment";
-		// Il faudra changer ce str en StringBuilder car la complexité de concatenation
-		// d'un String en java est trop élevé, je n'ai pas réussi à le faire
-		String str = "";
-		for (int i = 0; i < size; ++i) {
-			str += stuffs[i].toString();
-			str += "\r";
-		}
-		return str;
-		// Exemple : 36/64
-	}
-
 	public String showItem(int index) {
-		if (index >= size || index < 0)
+		if (stuffs[index] == null)
 			return "Out of range";
 		return stuffs[index].toString();
 	}
 
 	public Inventory addItem(Stuff stuff) {
 		for (int i = 0; i < size; ++i) {
-			if (stuffs[i].getName() == "Rien")
+			if (stuffs[i] != null)
 				continue;
-			stuff = stuffs[i];
+			stuffs[i] = stuff;
+			break;
 		}
 		return this;
 	}
@@ -60,10 +44,10 @@ public class Inventory {
 	public String toString() {
 		String str = "";
 		for (int i = 0; i < size; ++i) {
-			if (str.isEmpty())
+			if (stuffs[i] == null)
 				break;
-			str += stuffs[i].toString();
-			str += "\r";
+			str += i + " - " + stuffs[i].getName();
+			str += "\n";
 		}
 		return str;
 	}

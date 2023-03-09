6794d0b10eb3fdca035d5adb8ad66e9fcb09de85
Quentin <quentinb2004@gmail.com>
2023-03-09 17:57:12 +0100

    Fixes of the Cc stat for leather's stuff

--- RpgGame/src/equipements/boots/LeatherBoots.java
+++ RpgGame/src/equipements/boots/LeatherBoots.java
@@ -8,7 +8,6 @@ public class LeatherBoots extends Boots {
 		
 		this.bonusHp  = 2 ;
 		this.bonusDef = 2 ;
-		this.bonusCc  =  0;
 		
 		this.dropRate = 40.0 ;
 	}
@@ -19,8 +18,7 @@ public class LeatherBoots extends Boots {
 		return super.toString() +
 				"%s%n".formatted(name) +
 				"%-4d PV%n".formatted(bonusHp) +
-				"%-4d DEF%n".formatted(bonusDef) +
-				"%-4d CC%n".formatted(bonusCc);
+				"%-4d DEF%n".formatted(bonusDef);
 	}
 	
 }
